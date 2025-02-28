bd57af61238c637c720216e1c3ee0727836189b7
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-02-28 10:15:08 -0500

    Creacion del proyecto de Spring Boot

---
+++ microservice-test/src/main/java/com/example/microservicetest/model/Producto.java
@@ -0,0 +1,17 @@
+package com.example.microservicetest.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private double precio;
+    private int stock;
+}
---
+++ microservice-test/src/test/java/com/example/microservicetest/MicroserviceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
